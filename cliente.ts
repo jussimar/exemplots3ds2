5e2e2b074286e319db0e612412080e7c68994732
jussimar <jussimar@jussimarleal.com.br>
2019-09-02 14:51:18 -0300

    inserindo funcionario e cliente

---
+++ cliente.ts
@@ -0,0 +1,21 @@
+///<reference path="pessoa.ts" />
+namespace empresa{
+    class Cliente extends Pessoa{
+        private _codCliente:number;
+        private _credito:number;
+
+        public getCodCliente(){
+            return this._codCliente;
+        }
+        public setCodCliente(codigo:number){
+            this._codCliente = codigo;
+        }
+
+        public getCredito(){
+            return this._credito;
+        }
+        public setCredito(credito:number){
+            this._credito =  credito;
+        }
+    }
+}
---
+++ funcionario.ts
@@ -0,0 +1,21 @@
+///<reference path="pessoa.ts" />
+namespace empresa{
+    class Funcionario extends Pessoa{
+        private _codFuncionario:number;
+        private _setor:string;
+
+        public getCodFuncionario(){
+            return this._codFuncionario;
+        }
+        public setCodFuncionario(codigo:number){
+            this._codFuncionario = codigo;
+        }
+
+        public getSetor(){
+            return this._setor;
+        }
+        public setSetor(setor:string){
+            this._setor =  setor;
+        }
+    } 
+}
